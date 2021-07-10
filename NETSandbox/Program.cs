7fc540b24358b9897c731c97659bea204aee23d5
simasbakus <simas.bakus@gmail.com>
2021-07-10 16:29:08 +0300

    nuget fixes

--- NETSandbox/Program.cs
+++ NETSandbox/Program.cs
@@ -1,17 +1,10 @@
-﻿using NETSandbox.Entities;
-using Newtonsoft.Json;
-using NSubstitute;
-using System;
-using System.Collections.Generic;
-using System.IO;
-
-namespace NETSandbox
+﻿namespace NETSandbox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
